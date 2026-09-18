a3dcacc37ecf818df8797fc3ecda8c6747179a0e
Lester López <llopez-2022064@kinal.edu.gt>
2026-09-17 19:02:17 -0600

    feat: change the API URL

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
